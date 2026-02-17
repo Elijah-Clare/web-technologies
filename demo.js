7a0c268a9ee8f60d43cd8ddf2b583c8844744ed0
Mackenzie E Clare <mackenzie.e.clare@gmail.com>
2026-02-17 12:14:39 -0600

    Modified files to continue practicing with JavaScript

--- demo.js
+++ demo.js
@@ -1,6 +1,11 @@
+// Invoked when an event happens
 function my_function() {
     let firstName = prompt("What is your first name?");
+    console.log("firstName: " + firstName);
+
     let lastName = prompt("What is your last name?");
+    console.log("lastName: " + lastName);
+
     let fullName = firstName + ' ' + lastName;
 
     let userConfirmed = confirm("Hello! " + fullName + " Welcome to CSCI4410! Click OK to continue!");
@@ -13,3 +18,17 @@ function my_function() {
         console.log(fullName + " denied to continue.");
     }
 }
+
+// Invoke when called from JavaScript
+function greet() {
+    console.log("Hello, welcome to JavaScript!");
+}
+
+greet();
+
+// Invoked with an event listener
+function showMessage() {
+    alert("Button Clicked! Fucntion invoked using event listener");
+}
+
+document.getElementById("myButton").addEventListener("click", showMessage);
